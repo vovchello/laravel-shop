bf53b0ae6428b82d16642f10ce09fe96eb9cacaa
Dmitriy <dima@bytesoft.io>
2018-10-19 11:08:54 +0300

    image table migrations

---
+++ database/migrations/2018_10_17_161901_update_images_table_set_polymorphic_relation.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateImagesTableSetPolymorphicRelation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->renameColumn('product_id', 'imageable_id');
+            $table->string('imageable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->renameColumn('imageable_id', 'product_id');
+            $table->dropColumn('imageable_type');
+        });
+    }
+}
